0413bed9336f62e7880b3c80333a789ecd592cf6
damvanhoangbuu1 <damhoangbuu@gmail.com>
2022-05-26 20:40:23 +0700

    get-post method, parameter and body

---
+++ readme.md
@@ -0,0 +1,6 @@
+npm init //Tao project
+npm install express //Install express
+npm i nodemon --save-dev //autoreload when save code
+Method get lấy params bằng request.query
+Mehoad post lấy params bằng request.body
+Cần sử dụng middleware app.use(urlencoded), app.use(json)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.engine('.hbs',handlebars.engine({
   extname: '.hbs'
@@ -17,6 +21,16 @@ app.use(morgan('combined'))
 //routes
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/search', (req, res) => {
+  console.log(req.query);
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 })
 
 app.listen(port, () => {
